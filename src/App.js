e16a162cf5518b2353524fb738ba933df9397d50
OlesiaJS <Lessia92@i.ua>
2022-10-14 17:09:03 +0300

    add-amount-of-questions

--- src/App.js
+++ src/App.js
@@ -1,24 +1,39 @@
-
 import './components/interview.css';
+import { useState } from 'react';
 import Post from "./components/Post";
 
 function App({ interviewQuestion }) {
-
+  const countQuestion = interviewQuestion.length;
+  const [list, setList] = useState(interviewQuestion);
+  const [countQuestionKnow, setCounter] = useState(0);
+  const listQuestion = list.map((item, i) => {
+    return (
+      <Post
+        index={i + 1}
+        question={item.question}
+        url={item.URL}
+        answer={item.answer}
+        readCounter={0}
+        countQuestionKnow={countQuestionKnow}
+        handleKnowCounter={(countQuestionKnow) => {
+          if (countQuestionKnow <= countQuestion)
+            setCounter(countQuestionKnow);
+        }}
+      //interviewQuestionChange={(readCounter, question) => {
+      // const newInterviewQuestion = listQuestion.map(el){}
+      // setInterviewQuestion(newInterviewQuestion);
+      //}}
+      />
+    );
+  }
+  );
+  const sortQuestion = listQuestion.sort(
+    (a, b) => b.readCounter - a.readCounter
+  );
   return (
     <div className="wrapper">
-      {interviewQuestion.map((item, i) => {
-        return (
-          <Post
-            index={i + 1}
-            question={item.question}
-            url={item.URL}
-            answer={item.answer}
-            readCounter={0}
-          />
-        );
-      }
-      )}
-
+      <div className='itemTitle'>Ви відповіли на {countQuestionKnow}/{countQuestion} питань </div>
+      <div> {sortQuestion}</div>
     </div>
   );
 }
